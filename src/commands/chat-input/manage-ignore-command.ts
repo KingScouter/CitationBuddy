88630758cc9251090e4b2dcd45afd07cc406d7e5
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-06-03 00:17:16 +0200

    Added a new chat-command to list all messages marked as ignored

---
+++ src/commands/chat-input/manage-ignore-command.ts
@@ -0,0 +1,53 @@
+import {
+  APIChatInputApplicationCommandInteraction,
+  APIInteractionResponseChannelMessageWithSource,
+  InteractionResponseType,
+  MessageFlags,
+} from 'discord-api-types/v10';
+import { Response } from 'express';
+import { ServerConfig } from 'src/models';
+import { BaseChatInputCommand } from './base-chat-input-commands';
+import { ChannelUtils } from '../../channel-utils';
+import { InteractionResponseFlags } from 'discord-interactions';
+
+class ManageIgnoreCommand extends BaseChatInputCommand {
+  constructor() {
+    super('manage_ignore', 'Manage ignored messages');
+  }
+
+  protected async executeInternal(
+    interaction: APIChatInputApplicationCommandInteraction,
+    res: Response,
+    config: ServerConfig,
+  ): Promise<void> {
+    if (interaction.channel.id !== config.citeChannelId) {
+      const channel = await ChannelUtils.getChannel(config.citeChannelId);
+
+      res.send({
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: `Please execute the command within the configured citation channel: ${channel.name}`,
+          flags: InteractionResponseFlags.EPHEMERAL,
+        },
+      });
+
+      this.autoDeleteInitialResponse(interaction);
+      return;
+    }
+
+    const filteredMessages = config.excludedMessageIds.map(
+      (elem) =>
+        `https://discord.com/channels/${config.guildId}/${config.citeChannelId}/${elem}`,
+    );
+
+    res.send({
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: {
+        content: `Messages:\n${filteredMessages.join('\n')}`,
+        flags: MessageFlags.Ephemeral,
+      },
+    } as APIInteractionResponseChannelMessageWithSource);
+  }
+}
+
+export default new ManageIgnoreCommand();
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,19 +1,21 @@
-import SelectCiteChannelCommand from './chat-input/select-cite-channel-command';
-import ShowCiteChannelCommand from './chat-input/show-cite-channel-command';
-import AddCiteCommand from './chat-input/add-cite-command';
-import GetRandomCiteCommand from './chat-input/get-random-cite-command';
-import AddCiteModalCommand from './chat-input/add-cite-modal-command';
-import IgnoreCitationCommand from './message-command/ignore-citation-command';
-import UnignoreCitationCommand from './message-command/unignore-citation-command';
+import selectCiteChannelCommand from './chat-input/select-cite-channel-command';
+import showCiteChannelCommand from './chat-input/show-cite-channel-command';
+import addCiteCommand from './chat-input/add-cite-command';
+import getRandomCiteCommand from './chat-input/get-random-cite-command';
+import addCiteModalCommand from './chat-input/add-cite-modal-command';
+import ignoreCitationCommand from './message-command/ignore-citation-command';
+import unignoreCitationCommand from './message-command/unignore-citation-command';
+import manageIgnoreCommand from './chat-input/manage-ignore-command';
 
 const CHAT_INPUT_COMMANDS = [
-  SelectCiteChannelCommand,
-  ShowCiteChannelCommand,
-  AddCiteCommand,
-  GetRandomCiteCommand,
-  AddCiteModalCommand,
+  selectCiteChannelCommand,
+  showCiteChannelCommand,
+  addCiteCommand,
+  getRandomCiteCommand,
+  addCiteModalCommand,
+  manageIgnoreCommand,
 ];
 
-const MESSAGE_COMMANDS = [IgnoreCitationCommand, UnignoreCitationCommand];
+const MESSAGE_COMMANDS = [ignoreCitationCommand, unignoreCitationCommand];
 
 export { CHAT_INPUT_COMMANDS, MESSAGE_COMMANDS };
