2341e85f580f450ede8bb704e6c0955f02ca7080
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-06-13 22:09:40 +0200

    - Switched the workspace to a monorepo - Added an Angular frontend project

--- apps/citation-buddy-bot/jest.config.ts
+++ apps/citation-buddy-bot/jest.config.ts
@@ -1,15 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'citebot',
+  displayName: 'citation-buddy-bot',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/citebot',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  coverageDirectory: '../../coverage/apps/citation-buddy-bot',
 };
