fb16d1886996d866988fb5285c2896443c234708
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-06-30 20:47:21 +0200

    Moved the sidbar into its own component to always display it besides the router outlet

--- apps/citation-buddy/src/app/app.component.ts
+++ apps/citation-buddy/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidebarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'citation-buddy';
-}
+export class AppComponent {}
--- apps/citation-buddy/src/app/main-screen/main-screen.component.ts
+++ apps/citation-buddy/src/app/main-screen/main-screen.component.ts
@@ -1,18 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Signal,
   computed,
-  effect,
   inject,
   signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from '../authentication/authentication.service';
-import { AppConfig } from '../models';
 import { DiscordBackendService } from '../discord-backend/discord-backend.service';
-import { OAuth2Routes, OAuth2Scopes } from 'discord-api-types/v10';
-import { DiscordAuth } from '../models/discord-auth';
 
 @Component({
   selector: 'app-main-screen',
@@ -26,26 +21,9 @@ export class MainScreenComponent {
   private readonly authenticationService = inject(AuthenticationService);
   private readonly discordBackendService = inject(DiscordBackendService);
 
-  private readonly oauth2Scopes = `${OAuth2Scopes.Identify}+${OAuth2Scopes.Guilds}`;
-  private readonly oauth2Url = `${OAuth2Routes.authorizationURL}?client_id=${
-    AppConfig.CLIENT_ID
-  }&response_type=code&redirect_uri=${encodeURI(
-    AppConfig.BASE_REDIRECT_URI
-  )}&scope=${this.oauth2Scopes}`;
+  protected isLoggedIn = computed(this.authenticationService.isAuthenticated);
 
   protected backendData = signal<string[]>([]);
-
-  protected get currentUser(): Signal<DiscordAuth | null> {
-    return computed(() => this.authenticationService.userAuth());
-  }
-
-  protected onLoginButtonClick(): void {
-    window.open(this.oauth2Url, '_self');
-  }
-
-  protected onLogoutButtonClick(): void {
-    console.log('Should revoke token');
-  }
 
   protected async onGetChannels(): Promise<void> {
     const channels = await this.discordBackendService.getGuilds();
