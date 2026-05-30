b8971db5ee43895edeb5233e54147e41567e6392
Gabriel Ortner <ortner.gabriel@gmail.com>
2026-05-30 17:12:32 +0200

    Cleanup of initial code

--- apps/quote-buddy-bot/src/main.ts
+++ apps/quote-buddy-bot/src/main.ts
@@ -1,27 +1,14 @@
-import {
-  Client,
-  Collection,
-  Events,
-  GatewayIntentBits,
-  Partials,
-} from 'discord.js';
+import { GatewayIntentBits, Partials } from 'discord.js';
 import deployGlobalCommands from './deploy-commands';
-import MessageCommand from './models/message-command';
 import { EVENTS } from './events';
-import ApplicationCommand from './models/application-command';
 import { BOT_COMMANDS } from './commands';
-
-class CustomClient extends Client {
-  commands = new Collection<string, ApplicationCommand>();
-  msgCommands = new Collection<string, MessageCommand>();
-}
+import { DiscordClient } from './models/discord-client';
 
 async function runBot(): Promise<void> {
   await deployGlobalCommands();
 
-  // client = new CustomClient({ intents: [GatewayIntentBits.Guilds] });
   // Discord client object
-  global.client = new CustomClient({
+  global.client = new DiscordClient({
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
@@ -30,33 +17,10 @@ async function runBot(): Promise<void> {
     ],
     partials: [Partials.Channel],
   });
-  // global.client = Object.assign(
-  //   new Client({
-  //     intents: [
-  //       GatewayIntentBits.Guilds,
-  //       GatewayIntentBits.GuildMessages,
-  //       GatewayIntentBits.DirectMessages,
-  //       GatewayIntentBits.MessageContent,
-  //     ],
-  //     partials: [Partials.Channel],
-  //   }),
-  //   {
-  //     commands: new Collection<string, ApplicationCommand>(),
-  //     msgCommands: new Collection<string, MessageCommand>(),
-  //   }
-  // );
-
-  client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  });
-
-  // client.commands = new Collection();
 
   for (const command of BOT_COMMANDS) {
     client.commands.set(command.data.name, command);
   }
-
-  // const eventsList = [readyEvent, interactionCreateEvent, messageCreateEvent];
 
   for (const elem of EVENTS) {
     if (elem.once) {
@@ -67,8 +31,6 @@ async function runBot(): Promise<void> {
   }
 
   client.login(process.env.DISCORD_TOKEN);
-
-  // RegisterCommands();
 }
 
 runBot();
