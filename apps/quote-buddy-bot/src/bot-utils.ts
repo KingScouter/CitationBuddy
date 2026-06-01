dc3d94be077acc810b67de656c7f6305c6a08d34
Gabriel Ortner <ortner.gabriel@gmail.com>
2026-06-01 23:58:35 +0200

    Further improvements for quiz-management

--- apps/quote-buddy-bot/src/bot-utils.ts
+++ apps/quote-buddy-bot/src/bot-utils.ts
@@ -1,12 +1,30 @@
-import { ChatInputCommandInteraction } from 'discord.js';
+import {
+  ButtonInteraction,
+  ChatInputCommandInteraction,
+  CommandInteraction,
+  GuildMember,
+} from 'discord.js';
 
 export class BotUtils {
   static autoDeleteReply(
-    interaction: ChatInputCommandInteraction,
+    interaction:
+      | CommandInteraction
+      | ChatInputCommandInteraction
+      | ButtonInteraction,
     timeout = 5000
   ): void {
     setTimeout(async () => {
       await interaction.deleteReply();
     }, timeout);
+  }
+
+  static getUsername(
+    interaction:
+      | CommandInteraction
+      | ChatInputCommandInteraction
+      | ButtonInteraction
+  ): string {
+    const user = interaction.member as GuildMember;
+    return user.nickname ?? user.user.globalName ?? user.user.username;
   }
 }
